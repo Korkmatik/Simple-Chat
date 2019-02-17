26002162950151f73f2fc75fe325d4a10b5994dd
Korkmatik <9c54efc1-143b-4f5f-9d2f-f6f189583f0f@aleeas.com>
2019-02-17 17:08:45 +0100

    Extracted the client into a class

---
+++ Sandbox/SimpleClient.cpp
@@ -0,0 +1,75 @@
+#include "SimpleClient.hpp"
+
+#include <iostream>
+
+SimpleClient::SimpleClient(std::string ipAddress, int port)
+	: ipAddress(ipAddress), port(port), isSocketInitialized(false)
+{
+	WSADATA data;
+	WORD ver = MAKEWORD(2, 2);
+	int wsResult = WSAStartup(ver, &data);
+	if (wsResult != 0) {
+		std::cerr << "Can't start Winsock, Err #" << wsResult << std::endl; // TODO: throw exception
+		isWsaInitialized = false;
+	}
+	isWsaInitialized = true;
+}
+
+SimpleClient::~SimpleClient()
+{
+	if (isSocketInitialized)
+		closesocket(sock);
+
+	if (isWsaInitialized)
+		WSACleanup();
+}
+
+void SimpleClient::initialize()
+{
+	// Create socket
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET) {
+		std::cerr << "Can't create socket, Err #" << WSAGetLastError() << std::endl; // TODO: throw exception
+	}
+
+	// fill in a hint struct
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(port);
+	inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+}
+
+void SimpleClient::run()
+{
+	// connect to server
+	int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
+	if (connResult == SOCKET_ERROR) {
+		std::cerr << "Can't connect to server, Err #" << WSAGetLastError() << std::endl; //TODO: throw exception
+	}
+	isSocketInitialized = true;
+
+	// do while loop to send and receive data
+	char buf[4096];
+	std::string userInput;
+
+	do
+	{
+		// Prompt the user for some text
+		std::cout << "> ";
+		getline(std::cin, userInput);
+
+		if (userInput.size() > 0) {
+			// Send the text
+			int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+			if (sendResult != SOCKET_ERROR) {
+				//Wait for response
+				ZeroMemory(buf, 4096);
+				int bytesReceived = recv(sock, buf, 4096, 0);
+
+				if (bytesReceived > 0) {
+					// Echo response to console
+					std::cout << "SERVER> " << std::string(buf, 0, bytesReceived) << std::endl;
+				}
+			}
+		}
+	} while (userInput.size() > 0);
+}
